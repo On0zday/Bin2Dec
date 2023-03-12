b10b5274fbde235b79348aeeb95dade0483d80da
On0zday <cozzer@gmail.com>
2023-03-12 15:31:16 -0300

    finalizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,48 @@ const input = document.querySelector("input");
 const btn = document.querySelector("button");
 
 
+
+
+
+input.addEventListener("keypress", (event) => {
+
+	// limitar digito
+
+	if (String.fromCharCode(event.keyCode) != "1" && String.fromCharCode(event.keyCode) != "0") {
+		alert("Digite um numero entre 0 e 1");
+		event.preventDefault();
+	}
+});
+
+function calcConvert (numberBin) {
+	let newNumberDecimal = 0;
+
+
+	for (let ind = 0, p = (numberBin.length - 1); ind < numberBin.length; ind++, p--) {
+		const trans = parseInt(numberBin[ind]);
+		if (trans === 1) {
+			newNumberDecimal += Math.pow(2, p);
+		}
+	}
+
+	return newNumberDecimal;
+}
+
+
+function isValue (decimal) {
+	input.value = `>> ${decimal} <<`;
+}
+
+
+btn.addEventListener("click", (event) => {
+
+	event.preventDefault();
+
+
+	isValue(calcConvert(input.value));
+
+});
+
+
+
+
